7322f121031dd955f1d5c6f82b79d3f2cde25914
shivambhagat79 <shivambhagat2279+gitub@gmail.com>
2025-02-23 01:45:54 +0530

    Add gallery to dashboard.

--- lib/components/dashboard/dashboard.dart
+++ lib/components/dashboard/dashboard.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
-import 'package:hunger_games/components/common/heading_cut_card.dart';
 import 'package:hunger_games/components/dashboard/dashboard_actions.dart';
 import 'package:hunger_games/components/dashboard/dashboard_activities.dart';
+import 'package:hunger_games/components/dashboard/dashboard_gallery.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -20,6 +19,7 @@ class _DashboardState extends State<Dashboard> {
           children: [
             DashboardActivities(),
             DashboardActions(),
+            DashboardGallery(),
           ],
         ),
       ),
