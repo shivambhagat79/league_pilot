052ab3966cfea48a8a1e86634b67d05cdccb6b4b
Saharsh Saxena <saharshtarun@gmail.com>
2025-02-23 12:03:35 +0530

    sign in and sign up

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,1 +1,53 @@
-import 
+import "package:firebase_auth/firebase_auth.dart";
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anon
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email & password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // register with email & password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
